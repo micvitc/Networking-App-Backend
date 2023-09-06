3d6610edd50ebfa66e53251fdcab4c428d1810a5
Reuben-Stephen-John <reuben.sj2003@gmail.com>
2023-09-06 19:18:10 +0530

    adding allowed hosts

--- netbackend/settings.py
+++ netbackend/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['networking-app-backend.onrender.com']
+
 
 
 # Application definition
